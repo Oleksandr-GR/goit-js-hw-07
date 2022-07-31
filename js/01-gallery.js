d50a7ae3f82eb305b739a40dcf5b00e3ed9adb8d
Oleksandr Gryshko <alexandrgrishko@gmail.com>
2022-07-31 09:59:32 +0300

    Final commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.addEventListener('click', onClickEvent); 
+
+function createGallery(items) {
+    
+    return items.map(item => ` 
+    <div class="gallery__item">
+      <a class="gallery__link">
+       <img src="${item.preview}" alt="${item.description}" data-sourse="${item.original}" class="gallery__image">
+     </a>
+    </div> `).join('');
+}
+
+const gallery = createGallery(galleryItems);
+galleryContainer.innerHTML = gallery;
+
+function onClickEvent(event) {
+    
+    if (event.target.nodeName !== "IMG") { return };
+    
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.sourse}" width="800" height="600" >`);
+    instance.show();
+
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') { instance.close() }
+    })
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery(items) {
+    
+    return items.map(item => ` 
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join('');
+}
+
+const wholeGallery = createGallery(galleryItems);
+galleryContainer.innerHTML = wholeGallery;
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+gallery.on('show.simplelightbox');
+
