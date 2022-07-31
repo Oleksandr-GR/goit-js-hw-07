a6aff70d7a68db4640a5dd597a02d51c37090f3b
Oleksandr Gryshko <alexandrgrishko@gmail.com>
2022-07-31 10:28:36 +0300

    final 31.07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,9 +6,11 @@ const galleryContainer = document.querySelector('.gallery');
 function createGallery(items) {
     
     return items.map(item => ` 
-    <a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>`).join('');
+    <li>
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>`).join('');
 }
 
 const wholeGallery = createGallery(galleryItems);
